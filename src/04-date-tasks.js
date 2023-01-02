1f1dfe5ec00713e5a6ef4ed8549d502ffc4f78d8
Slava137 <slava13071995@gmail.com>
2023-01-03 02:36:16 +0300

    feat: add the last two solutions for date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -77,8 +77,11 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const dif = endDate - startDate;
+  const newDate = new Date(dif).toISOString();
+
+  return newDate.slice(11, newDate.length - 1);
 }
 
 
@@ -98,8 +101,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours();
+  const minute = date.getMinutes();
+
+  let angle = Math.abs(0.5 * (60 * (hour % 12) - 11 * minute));
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return (angle * Math.PI) / 180;
 }
 
 
